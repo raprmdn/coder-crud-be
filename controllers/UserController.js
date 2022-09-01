478345fd025bfabc76ad8a17970c0d4e7a327d87
raprmdn <rafiputraramadhan14@gmail.com>
2022-09-01 18:47:04 +0700

    find, create, update, delete endpoint.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,3 +9,51 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const findUserById = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const createUser = async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.status(201).json({ message: "User created successful." });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "User updated successful." });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "User deleted successful." });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+}
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { getUsers } from "../controllers/UserController.js";
+import {
+    getUsers, findUserById, createUser,
+    updateUser, deleteUser
+} from "../controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', findUserById);
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
